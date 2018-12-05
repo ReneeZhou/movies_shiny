29535fe8fb6fcfc9d1f23e0664cc857985a28870
Renee <shanju.chou@gmail.com>
2018-12-04 22:35:07 -1200

    Divided data sets according to category column and saved as separate objs

--- workflow_2.R
+++ workflow_2.R
@@ -8,10 +8,14 @@ principals <- read_tsv("title_principals.tsv")
 
 # Divide principals data set according to category column -----------------
 # Have a look at the categories
-principals %>% distinct(category)
+category <- principals %>% distinct(category)
+
+
+# Division
 self <- principals %>% filter(category == "self")
 director <- principals %>% filter(category == "director")
 cinematographer <- principals %>% filter(category == "cinematographer")
+composer <- principals %>% filter(category == "composer")
 producer <- principals %>% filter(category == "producer")
 editor <- principals %>% filter(category == "editor")
 actor <- principals %>% filter(category == "actor")
@@ -20,3 +24,19 @@ writer <- principals %>% filter(category == "writer")
 production_designer <- principals %>% filter(category == "production_designer")
 archive_footage <- principals %>% filter(category == "archive_footage")
 archive_sound <- principals %>% filter(category == "archive_sound")
+
+
+# Remove the original, save and clean up the workspace
+rm(principals)
+save(self, file = "self")
+save(director, file = "director")
+save(cinematographer, file = "cinematographer")
+save(composer, file = "composer")
+save(producer, file = "producer")
+save(editor, file = "editor")
+save(actor, file = "actor")
+save(actress, file = "actress")
+save(writer, file = "writer")
+save(production_designer, file = "production_designer")
+save(archive_footage, file = "archive_footage")
+save(archive_sound, file = "archive_sound")
