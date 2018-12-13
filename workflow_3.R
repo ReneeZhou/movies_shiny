4f240a6fb793c7f758eb5dd66fa17c042d93bacf
Renee <shanju.chou@gmail.com>
2018-12-13 23:42:14 +1100

    Tidied name_example, saved as name_prof

--- workflow_3.R
+++ workflow_3.R
@@ -6,3 +6,11 @@ library(tidyverse)
 name <- read_tsv("name_basics.tsv")
 name_example <- name %>% slice(1:100) # use this tibble to think about how to break name dataset
 rm(name)
+
+
+# Seperate into multiple data frames --------------------------------------
+name_prof <- name_example %>% 
+  select(nconst, primaryName, primaryProfession) %>% 
+  rename(name = primaryName, profession = primaryProfession) %>%  # rename columns
+  separate_rows(profession, sep = ",")
+  
