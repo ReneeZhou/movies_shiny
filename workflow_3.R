7637e622c35e955e5e76de185725a6a826937ebc
Renee <shanju.chou@gmail.com>
2018-12-13 21:06:25 +0800

    Divided name into 3 data sets, did basic tidying

--- workflow_3.R
+++ workflow_3.R
@@ -4,13 +4,24 @@ library(tidyverse)
 
 # Read in name data set ---------------------------------------------------
 name <- read_tsv("name_basics.tsv")
-name_example <- name %>% slice(1:100) # use this tibble to think about how to break name dataset
-rm(name)
+name_example <- name %>% slice(1:100) # example data set
 
 
-# Seperate into multiple data frames --------------------------------------
-name_prof <- name_example %>% 
+# Divide and tidy ---------------------------------------------------------
+name_profession <- name %>% 
   select(nconst, primaryName, primaryProfession) %>% 
-  rename(name = primaryName, profession = primaryProfession) %>%  # rename columns
+  rename(name = primaryName, profession = primaryProfession) %>% 
   separate_rows(profession, sep = ",")
-  
+
+
+name_known <- name %>% 
+  select(nconst, primaryName, knownForTitles) %>% 
+  rename(name = primaryName, known = knownForTitles)
+
+
+name_age <- name %>% 
+  select(nconst, primaryName, birthYear, deathYear) %>% 
+  rename(name = primaryName, birth = birthYear, death = deathYear)
+
+
+rm(name) 
