12f14b423fd53f683aecf3545b74945fda34cf51
Renee <shanju.chou@gmail.com>
2018-12-16 18:20:05 -1200

    Created and cleaned name_age & name_age_arrange; added an exploratory plot of age of all professions

--- workflow_3.R
+++ workflow_3.R
@@ -18,15 +18,19 @@ name_profession <- name %>%
 
 name_known <- name %>% 
   select(nconst, primaryName, knownForTitles) %>% 
-  rename(name = primaryName, known = knownForTitles) %>% 
+  rename(name = primaryName, known = knownForTitles) 
   # this line to check if there's NAs in known col - if so, remove them
   # separate_rows(known, sep = ",") # this will expand the rectangle a lot
 
 
+# Only containts information of professionals who are ceased
 name_age <- name %>% 
   select(nconst, primaryName, birthYear, deathYear) %>% 
-  rename(name = primaryName, birth = birthYear, death = deathYear)
-
+  rename(name = primaryName, birth = birthYear, death = deathYear) %>% 
+  mutate(birth = as.integer(birth), death = as.integer(death), 
+         age = death - birth) %>% 
+  filter(!is.na(birth), !is.na(death), !is.na(age))
+  
 
 rm(name) 
 
@@ -35,3 +39,16 @@ rm(name)
 prof_count <- name_profession %>% count(profession) %>% arrange(desc(n))
 prof_count %>% ggplot(aes(x = n, y = reorder(profession, n))) +
   geom_point()
+
+
+name_age_arrange <- name_age %>%
+  arrange(desc(age)) %>% 
+  {.[1,4] <- 1865; .[1,5] <- 53; . } %>% # if without . at the very end, it won't save the entire tibble
+  # Corrected data value after some research
+  # name_age %>%  {.[1,4] <- 1865; .} is equivalent to 
+  # name_age %T>% {.[1,4] <- 1865} magrittr tree operator
+  filter(age > 0) %>% # remove negative age and age 0
+  arrange(age)
+
+name_age_arrange %>% ggplot(aes(x = age)) +
+  geom_bar()
