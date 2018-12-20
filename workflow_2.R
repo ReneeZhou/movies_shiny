434665afdddbc99daf07e9db27ab878ab12f42df
Renee <shanju.chou@gmail.com>
2018-12-19 20:38:39 -1200

    Replaced readRDS(), saveRDS() with readr::read_rds(), readr::write_rds() accordingly for consistency

--- workflow_2.R
+++ workflow_2.R
@@ -8,14 +8,14 @@ principals <- read_tsv("title_principals.tsv")
 
 # Divide principals data set according to category column -----------------
 category_var <- unique(principals$category)
-saveRDS(category_var, file = "category_var.rds") # saving for later use
+write_rds(category_var, path = "category_var.rds", compress = "gz") # saving for later use
 
 
 # # Looping over each category to save as different files -----------------
 for (i in 1:length(category_var)) {
   obj <- principals %>% filter(category == category_var[i])
   nam <- category_var[i]
-  saveRDS(obj, file = paste0("raw_", nam, ".rds"))
+  write_rds(obj, path = paste0("raw_", nam, ".rds"), compress = "gz")
   rm(obj, nam, i)
 }
 
@@ -30,7 +30,7 @@ for (i in 1:length(category_var)) {
   # can change length to load in a few files only 
   # use 11:12 as the smallest files
   nam <- category_var[i]
-  obj <- readRDS(paste0("raw_", nam, ".rds"))
+  obj <- read_rds(paste0("raw_", nam, ".rds"))
   
   if (length(unique(obj$job)) == 1) {
     if (unique(obj$job) == "\\N") {
@@ -57,7 +57,7 @@ for (i in 1:length(category_var)) {
   # Assign the namce accordingly
   assign(nam, obj)
   # Overwrite files
-  saveRDS(obj, file = paste0("clean_", nam, ".rds"))
+  write_rds(obj, path = paste0("clean_", nam, ".rds"), compress = "gz")
   # Remove redundant objs
   rm(i, nam, obj)
 }
